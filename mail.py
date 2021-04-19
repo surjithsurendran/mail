b053530a691b802b591f6534510448df66f5ec2d
surjithsurendran <81320434+surjithsurendran@users.noreply.github.com>
2021-04-19 17:13:54 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,7 +1,7 @@
 import smtplib
 
-server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-server.login("surjith6666@gmail.com", "9846026274")
-server.sendmail("surjithsurendranp@gmail.com", "Hello, How are you?")
+server = smtplib.SMTP_SSL("smtp.54.234.55.202.com", 465)
+server.login("surjith@mail.surjithp.com", "1234")
+server.sendmail("devops@mail.surjith1.com", "Hello, How are you?")
 
 server.quit()
